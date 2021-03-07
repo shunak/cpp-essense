cb06222d22fa44bab0e31c2efe73ebe06efefbda
shunak <shuna966@gmail.com>
2021-03-07 21:26:35 +0900

    added

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "int	: " << sizeof (int) << endl;
+	cout << "char	: " << sizeof (char) << endl;
+	cout << "double	: " << sizeof (double) << endl;
+	cout << "int*	: " << sizeof (int*) << endl;
+	cout << "char*	: " << sizeof (char*) << endl;
+	cout << "double*	: " << sizeof (double*) << endl;
+
+	return 0;
+}
