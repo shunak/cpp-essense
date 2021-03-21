5e3db448b17fb40637ff2fd6203e54b5ebcd0e7e
shunak <shuna966@gmail.com>
2021-03-21 15:05:07 +0900

    added

---
+++ static-argument.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void Func(int a=0){
+	cout << &a << endl;
+}
+
+void Func2(){
+	Func();
+}
+
+
+int main(){
+	Func();
+	Func2();
+	Func();
+
+	return 0;
+}
