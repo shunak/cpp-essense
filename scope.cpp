1a7766c7ed29be210addbd85023fa81ab8dc6e46
shunak <shuna966@gmail.com>
2021-03-23 16:42:43 +0900

    added

---
+++ scope.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a =1;
+
+int main(){
+	cout << a << ":" << &a << endl;
+
+	int a =2;
+	cout << a << ":" << &a << endl;
+
+	{
+		int a =3;
+		cout << a << ":" << &a << endl;
+	}
+
+	cout << a << ":" << &a << endl;
+
+	return 0;
+
+}
