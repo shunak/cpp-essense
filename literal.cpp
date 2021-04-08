2334274fbe7f581c8a597b8302e7e2917179e815
shunak <shuna966@gmail.com>
2021-04-08 11:39:10 +0900

    addeed

--- literal.cpp
+++ literal.cpp
@@ -4,9 +4,42 @@ using namespace std;
 
 enum ECompare
 {
-	LESSTHAN =0;
-	EQUALTO =1;
-	GREATERTHAN =2;
+	LESSTHAN =0,
+	EQUALTO =1,
+	GREATERTHAN =2,
 };
 
 
+
+ECompare Compare(int a, int b)
+{
+	return (a < b) ? LESSTHAN: (a > b) ? GREATERTHAN : EQUALTO;
+}
+
+bool Compare()
+{
+	const char* pszResult[]={
+		"Former value is less than latter.",
+		"Both value is equal.",
+		"Former value is greater than latter.",
+	};
+
+	int a, b;
+
+	cout << "Input two numbers > " << flush;
+	cin >> a >> b;
+
+	if(a==-1)
+		return false;
+	cout << pszResult[Compare(a,b)] << endl;
+
+	return true;
+}
+
+
+int main()
+{
+	while(Compare());
+
+	return 0;
+}
