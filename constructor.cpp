941cd641e5b361ac9cdc7acff817fb6950ade5a8
shunak <shuna966@gmail.com>
2021-04-13 15:15:07 +0900

    added

---
+++ constructor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class CInt
+{
+	public:
+		int m_num;
+		CInt(); //Constructor
+		void Disp();
+};
+
+
+// Constructor
+CInt::CInt()
+{
+	m_num=0;
+}
+
+
+void CInt::Disp()
+{
+	cout << m_num << endl;
+}
+
+int main()
+{
+	CInt a, b, c, d;
+
+	a.Disp();
+	b.Disp();
+	c.Disp();
+	d.Disp();
+
+	return 0;
+
+}
+
+
+
+
+
