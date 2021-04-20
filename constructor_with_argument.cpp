242e551bca3505316dff021c0bff808ba20807da
shunak <shuna966@gmail.com>
2021-04-20 11:49:43 +0900

    added

--- constructor_with_argument.cpp
+++ constructor_with_argument.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-using namepsace	std;
+using namespace	std;
 
 
 class CInt
 {
+public:
+		int m_num;
+		CInt();
+		CInt(int num);
+		void Disp();
+};
 
+//constructor
+CInt::CInt() {m_num=0;}
+CInt::CInt(int num){m_num=num;}
+
+
+//display member variable
+void CInt::Disp(){cout << m_num << endl;}
+
+int main()
+{
+	CInt a, b(200), c(40);
+
+	a.Disp();
+	b.Disp();
+	c.Disp();
+
+	return 0;
 }
