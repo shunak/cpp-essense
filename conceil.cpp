a2ebe498b79ecfc7996f17a461bcb8cd2c1ef083
shunak <shuna966@gmail.com>
2021-04-22 17:55:24 +0900

    added

--- conceil.cpp
+++ conceil.cpp
@@ -1,6 +1,16 @@
 #include <memory.h>
 
 class CIntArray{
+	public:
+		int m_anum[100];
 
+		CIntArray();
+};
 
+//Constructor
+CIntArray::CIntArray()
+{
+	//memset function is a function which assign data by byte unit.
+	memset(m_anum,0,sizeof m_anum);
 }
+
