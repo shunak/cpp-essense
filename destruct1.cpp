5a04d042689197c47af25e58bd53e864c6e05668
shunak <shuna966@gmail.com>
2021-04-23 22:32:35 +0900

    added

--- destruct1.cpp
+++ destruct1.cpp
@@ -7,3 +7,25 @@ void Visss(const int num)
 }
 
 
+CIntArray a(10);
+
+
+int main()
+{
+	CIntArray b(20);
+	Viss(1);
+
+	CIntArray c(30);
+	Viss(2);
+
+	{
+		CIntArray(40);
+		Viss(3);
+	}
+	Viss(4);
+
+	return 0;
+
+
+
+}
