f71871acd902c710d45c02f7616b6b97a17328b0
shunak <shuna966@gmail.com>
2021-04-27 17:46:40 +0900

    added

--- intarray.cpp
+++ intarray.cpp
@@ -6,5 +6,24 @@
 // constructor
 CIntArray::CIntArray(const int nNumOf)
 {
+	m_pnum = new int[nNumOf];
+	if(m_pnum == NULL)
+		m_nNumOf = 0;
+	else
+	{
+		m_nNumOf = nNumOf;
+		memset(m_pnum, 0, nNumOf * sizeif *m_pnum);
+	}
 
+	cout << "Constructor is called."
+		<< "Num of the elements is " << m_nNumOf << ". " << endl;
 }
+
+
+// Destructor
+CIntArray::~CIntArray()
+{
+}
+
+
+
